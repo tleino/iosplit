4d3ecc90e6842cce58764677208bcf8726c16a3a
Tommi Leino <namhas@gmail.com>
2021-10-24 01:54:04 +0300

    Change to unified stream with separate cursors
    Previously input and output was separated to their own boxes,
    with input always at the bottom of the screen, and output did
    not mess up with input in any way.
    
    However, the same can be achieved by using separate cursors
    for input and output, so that unexpected output will not
    interfere with input.
    
    At the same separate input buffer does not need to be kept
    for editing, but the whole stream can be made editable.

--- iosplit.c
+++ iosplit.c
@@ -4,6 +4,8 @@
 #include <err.h>
 #include <unistd.h>
 #include <stdlib.h>
+#include <assert.h>
+#include <string.h>
 
 #ifdef __OpenBSD__
 #include <util.h>
@@ -19,21 +21,249 @@
 #include <string.h>
 #include <termios.h>
 
-int
-recalc_col(const char *s)
-{
-	int col;
+struct view {
+	struct row *top;
+};
 
-	col = 0;
-	while (*s != '\0') {
-		if (*s == '\t')
-			col += (8 - (col % 8));
-		else
-			col++;
-		s++;
+enum {
+	OUTPUT_CURSOR,
+	INPUT_CURSOR
+};
+
+struct cursor {
+	struct row *row;
+	size_t col;
+	int type;
+};
+
+struct buffer {
+	struct cursor input;
+	struct cursor output;
+	struct row *first;
+};
+
+#define CMD_ROW (1 << 0)
+
+struct row {
+	char *text;
+	int flags;
+	size_t text_alloc;
+	size_t text_len;
+	size_t prompt_len;
+	struct row *prev;
+	struct row *next;
+	struct buffer *buffer;
+};
+
+#define ROW_INITIAL_ALLOC (40)
+
+void insert_text(struct cursor *, struct cursor *, char *);
+void draw_buffer(WINDOW *, struct buffer *);
+void init_buffer(struct buffer *);
+struct row *add_row(struct buffer *, struct row *);
+int is_trailing_cursor(struct cursor *, struct cursor *);
+struct row *find_row_flags(struct row *, int);
+void remove_row(struct row *);
+void clear_to(struct row *, struct row *);
+void set_cursor(struct cursor *, struct row *, size_t);
+
+int
+is_trailing_cursor(struct cursor *candidate, struct cursor *reference)
+{
+	if (candidate->row == reference->row &&
+	    candidate->col >= reference->col)
+		return 1;
+
+	return 0;
+}
+
+struct row *
+find_row_flags(struct row *np, int flags)
+{
+	for (; np != NULL; np = np->next)
+		if (np->flags & flags)
+			return np;
+
+	return NULL;
+}
+
+void
+remove_row(struct row *np)
+{
+	if (np->prev)
+		np->prev->next = np->next;
+	if (np->next)
+		np->next->prev = np->prev;
+	if (np == np->buffer->first)
+		np->buffer->first = np->next;
+	free(np);
+}
+
+void
+clear_to(struct row *np, struct row *last)
+{
+	struct row *next;
+
+	for (; np != NULL && np != last; np = next) {
+		next = np->next;
+		remove_row(np);
+	}
+}
+
+void
+insert_text(struct cursor *cursor, struct cursor *other, char *text)
+{
+	struct row *row;
+
+	assert(cursor != NULL);
+	assert(cursor->row != NULL);
+
+	while (*text != '\0') {
+		if (*text == '\r') {
+			text++;
+			continue;
+		}
+		if (*text == '\n') {
+			if (cursor->type == OUTPUT_CURSOR)
+				cursor->row->prompt_len = 0;
+
+			row = add_row(cursor->row->buffer, cursor->row);
+
+			if (is_trailing_cursor(other, cursor)) {
+				cursor->row = other->row = row;
+				cursor->col = other->col = 0;
+			} else {
+				cursor->row = row;
+				cursor->col = 0;
+			}
+			text++;
+			continue;
+		}
+
+		if (cursor->row->text_len == cursor->row->text_alloc) {
+			if (cursor->row->text_alloc == 0)
+				cursor->row->text_alloc = ROW_INITIAL_ALLOC;
+			else
+				cursor->row->text_alloc *= 2;
+			cursor->row->text = realloc(cursor->row->text,
+			    cursor->row->text_alloc * sizeof(char));
+			if (cursor->row->text == NULL)
+				err(1, "realloc");
+		}
+		if (cursor->col > cursor->row->text_len)
+			cursor->col = cursor->row->text_len;
+
+		if (is_trailing_cursor(other, cursor))
+			other->col++;
+
+		cursor->row->text_len++;
+		memmove(&cursor->row->text[cursor->col+1],
+		    &cursor->row->text[cursor->col],
+		    cursor->row->text_len - cursor->col);
+		cursor->row->text[cursor->col++] = *text++;
+
+		if (cursor->type == OUTPUT_CURSOR)
+			cursor->row->prompt_len = cursor->col;
+	}
+}
+
+void
+draw_buffer(WINDOW *win, struct buffer *buffer)
+{
+	struct row *np;
+	size_t linenum, i;
+
+	assert(buffer != NULL);
+
+	linenum = 0;
+	for (np = buffer->first; np != NULL; np = np->next) {
+		wmove(win, linenum, 0);
+		wclrtoeol(win);
+		wattron(win, A_REVERSE);
+		mvwprintw(win, linenum, 0, "%3d %3d/%3d %3d %c",
+		    linenum+1, np->text_len,
+		    np->text_alloc, np->prompt_len,
+		    np->flags & CMD_ROW ? 'c' : '-');
+		wattroff(win, A_REVERSE);
+		waddch(win, ' ');
+		for (i = 0; i < np->text_len; i++) {
+			if (np->text[i] == '\r')
+				continue;
+			if (buffer->input.row == np && buffer->input.col == i)
+				wattron(win, COLOR_PAIR(1));
+			if (buffer->output.row == np && buffer->output.col == i)
+				wattron(win, COLOR_PAIR(2));
+			waddch(win, np->text[i]);
+
+			if (buffer->input.row == np && buffer->input.col == i)
+				wattroff(win, COLOR_PAIR(1));
+			if (buffer->output.row == np && buffer->output.col == i)
+				wattroff(win, COLOR_PAIR(2));
+		}
+		if (buffer->input.row == np && buffer->input.col == i)
+			wattron(win, COLOR_PAIR(1));
+		else if (buffer->output.row == np && buffer->output.col == i)
+			wattron(win, COLOR_PAIR(2));
+
+		waddch(win, '<');
+
+		if (buffer->input.row == np && buffer->input.col == i)
+			wattroff(win, COLOR_PAIR(1));
+		else if (buffer->output.row == np && buffer->output.col == i)
+			wattroff(win, COLOR_PAIR(2));
+
+		linenum++;
+	}
+	wclrtobot(win);
+	wrefresh(win);
+}
+
+void
+set_cursor(struct cursor *cursor, struct row *row, size_t col)
+{
+	cursor->col = col;
+	cursor->row = row;
+}
+
+void
+init_buffer(struct buffer *buffer)
+{
+	assert(buffer != NULL);
+
+	buffer->first = add_row(buffer, NULL);
+	set_cursor(&buffer->input, buffer->first, 0);
+	set_cursor(&buffer->output, buffer->first, 0);
+	buffer->input.type = INPUT_CURSOR;
+	buffer->output.type = OUTPUT_CURSOR;
+}
+
+struct row *
+add_row(struct buffer *buffer, struct row *after)
+{
+	struct row *row;
+
+	assert(buffer != NULL);
+
+	row = calloc(1, sizeof(struct row));
+	if (row == NULL)
+		err(1, "calloc");
+	row->buffer = buffer;
+
+	if (after == NULL) {
+		row->next = buffer->first;
+		if (buffer->first != NULL)
+			buffer->first->prev = row;
+		buffer->first = row;
+	} else {
+		if (after->next) {
+			after->next->prev = row;
+			row->next = after->next;
+		}
+		after->next = row;
+		row->prev = after;
 	}
 
-	return col;
+	return row;
 }
 
 int
@@ -41,15 +271,17 @@ main(int argc, char *argv[])
 {
 	WINDOW *win, *owin, *iwin, *swin;
 	int ch;
-	char buf[4096 + 1], *p, ibuf[4096 + 1], *ip;
+	char buf[4096 + 1], *p, ibuf[4096 + 1];
 	pid_t pid;
 	int fd;
 	int n;
 	struct pollfd pfd[2];
 	int nready;
-	unsigned char c;
-	int nlines = 0;
 	int col = 0;
+	static struct buffer buffer;
+	struct row *row;
+
+	init_buffer(&buffer);
 
 	p = buf;
 	*p = '\0';
@@ -57,6 +289,11 @@ main(int argc, char *argv[])
 	setlocale(LC_ALL, "");
 
 	win = initscr();
+
+	start_color();
+
+	init_pair(1, COLOR_WHITE, COLOR_GREEN);
+	init_pair(2, COLOR_WHITE, COLOR_RED);
 
 	owin = newwin(getmaxy(win) - 2, getmaxx(win), 0, 0);
 	swin = newwin(1, getmaxx(win), getmaxy(win) - 2, 0);
@@ -68,6 +305,7 @@ main(int argc, char *argv[])
 	idlok(owin, true);
 	keypad(iwin, true);
 	keypad(win, true);
+	keypad(owin, true);
 
 	whline(swin, '-', getmaxx(win));
 
@@ -113,45 +351,81 @@ main(int argc, char *argv[])
 				write(fd, &ch, 1);
 				continue;
 			}
-			switch (ch) {
-			case '\t':
-				*p++ = '\t';
-				*p = '\0';
-				col = recalc_col(buf);
-				if (col > 79) {
-					*--p = '\0';
-					col = recalc_col(buf);
-				}
-				wmove(iwin, 0, col);
-				wclrtoeol(iwin);
-				break;
-			case '\n':
-				*p++ = (unsigned char) ch;
-				*p = '\0';
 
-				n = write(fd, buf, strlen(buf));
-				p = buf;
-				nlines = 1;
-				waddch(iwin, '\n');
-				col = 0;
+			switch (ch) {
+			case '\n':
+				if (buffer.input.row->text) {
+					row = buffer.input.row;
+					write(fd,
+					    &row->text[row->prompt_len],
+					    row->text_len - row->prompt_len);
+					write(fd, "\n", 1);
+					free(row->text);
+					row->flags |= CMD_ROW;
+					row->text = NULL;
+					row->text_alloc = 0;
+					row->text_len = 0;
+					row->prompt_len = 0;
+					buffer.input.col = 0;
+				}
+				buffer.output.row = buffer.input.row;
+				buffer.output.col = buffer.input.col;
+
+				clear_to(row->next, find_row_flags(row->next,
+				    CMD_ROW));
+				break;
+			case KEY_UP:
+				row = buffer.input.row;
+				row = buffer.input.row = row->prev;
+				if (row == NULL)
+					row = buffer.input.row = buffer.first;
+				if (buffer.input.col >= row->text_len)
+					buffer.input.col = row->text_len;
+				draw_buffer(owin, &buffer);
+				break;
+			case KEY_DOWN:
+				row = buffer.input.row;
+				if (row->next != NULL)
+					row = buffer.input.row = row->next;
+				if (buffer.input.col >= row->text_len)
+					buffer.input.col = row->text_len;
+				draw_buffer(owin, &buffer);
+				break;
+			case KEY_LEFT:
+				if (buffer.input.col > 0)
+					buffer.input.col--;
+				draw_buffer(owin, &buffer);
+				break;
+			case KEY_RIGHT:
+				row = buffer.input.row;
+				if (buffer.input.col < row->text_len)
+					buffer.input.col++;
+				draw_buffer(owin, &buffer);
 				break;
 			case KEY_BACKSPACE:
-				if (col > 0 && p > buf) {
-					*--p = '\0';
-					col = recalc_col(buf);
-					wmove(iwin, 0, col);
-					wclrtoeol(iwin);
+				row = buffer.input.row;
+				if (buffer.input.col > 0) {
+					row->text_len--;
+
+					if (buffer.input.col <= row->prompt_len)
+					    row->prompt_len--;
+
+					if (is_trailing_cursor(&buffer.output,
+					    &buffer.input))
+						buffer.output.col--;
+					buffer.input.col--;
+
+					if (row->text != NULL)
+						row->text[row->text_len] = '\0';
 				}
+				draw_buffer(owin, &buffer);
 				break;
 			default:
-				if (col < 80-1) {
-					if (isprint(ch) || iscntrl(ch)) {
-						*p++ = (unsigned char) ch;
-						*p = '\0';
-					}
-					col++;
-					waddch(iwin, ch);
-				}
+				ibuf[0] = ch;
+				ibuf[1] = '\0';
+				insert_text(&buffer.input, &buffer.output,
+				    ibuf);
+				draw_buffer(owin, &buffer);
 				break;
 			}
 			mvwprintw(swin, 0, 72-1, "%3d", col + 1);
@@ -162,22 +436,8 @@ main(int argc, char *argv[])
 			n = read(fd, ibuf, sizeof(ibuf));
 			if (n > 0) {
 				ibuf[n] = '\0';
-				ip = ibuf;
-
-				if (nlines)
-					wattron(owin, A_REVERSE);
-
-				while ((c = *ip++) != '\0') {
-					if (c == '\r') {
-						if (nlines) {
-							nlines = 0;
-							wattroff(owin,
-							    A_REVERSE);
-						}
-						continue;
-					}
-					waddch(owin, c);
-				}
+				insert_text(&buffer.output, &buffer.input,
+				    ibuf);
 			} else {
 				if (n < 0) {
 					endwin();
@@ -185,6 +445,7 @@ main(int argc, char *argv[])
 				}
 				break;
 			}
+			draw_buffer(owin, &buffer);
 			wrefresh(owin);
 			wrefresh(iwin);
 		}
